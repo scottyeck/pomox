e55d00a42c9055d132c8b44b45ce1abc517f39a0
Scotty Eckenthal <scott.eckenthal@gmail.com>
2026-01-07 00:31:57 -0500

    Raycast params

--- raycast/pomox-start.sh
+++ raycast/pomox-start.sh
@@ -3,6 +3,12 @@
 # @raycast.schemaVersion 1
 # @raycast.title Pomox Start
 # @raycast.mode silent
+# @raycast.argument1 { "type": "text", "placeholder": "minutes", "optional": true }
 
 source "${NVM_DIR:-$HOME/.nvm}/nvm.sh"
-pomox start
+
+if [ -n "$1" ]; then
+  pomox start -d "$1"
+else
+  pomox start
+fi
